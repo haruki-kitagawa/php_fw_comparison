235e76a514805ad69dfcc34af489a70d71251ee0
Haruki <k.haruki.zephyranthes@gmail.com>
2026-06-15 10:44:49 +0900

    ログイン機能・画面生成

--- lv_app/routes/web.php
+++ lv_app/routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Http\Controllers\DetailController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// http://localhost:8001/products
-Route::get('/products', [ProductController::class, 'index'])->name('products');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// http://localhost:8001/products/detail
-Route::get('/detail/{id}', [DetailController::class, 'index'])->name('detail');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
