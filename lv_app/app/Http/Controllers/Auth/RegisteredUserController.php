95421b171320c03d41bcb6e82d05d428bac0a941
Haruki <k.haruki.zephyranthes@gmail.com>
2026-06-22 10:06:48 +0900

    新規登録後のリダイレクト先をdashboardからproducts(商品一覧)に変更

--- lv_app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ lv_app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,6 +46,7 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        // ログイン後は商品一覧に遷移
+        return redirect(route('products', absolute: false));
     }
 }
