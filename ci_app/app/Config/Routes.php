7733aeb94b7dd06fb507d6b44d0d5e31e1084293
Haruki <k.haruki.zephyranthes@gmail.com>
2026-07-06 11:15:36 +0900

    ルーティング修正(ログインチェック実装)

--- ci_app/app/Config/Routes.php
+++ ci_app/app/Config/Routes.php
@@ -5,9 +5,21 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 
-// http://localhost:8002/products
-$routes->get('products', 'Product::index');
+// 1. トップページ「/」にアクセスされたら、自動的に「/products」へリダイレクト
+$routes->get('/', function() {
+    return redirect()->to('/products');
+});
 
-$routes->get('detail/(:num)', 'Detail::index/$1');
+// 2. ログインチェック必須ページ（products グループ）
+$routes->group('products', ['filter' => 'session'], function ($routes) {
+    
+    // これで http://localhost:8002/products にアクセスした時に Product::index が動きます
+    $routes->get('', 'Product::index');
+
+    // http://localhost:8002/products/detail/1
+    $routes->get('detail/(:num)', 'Detail::index/$1');
+});
+
+// Shieldの認証ルーティング（/login や /register など）を自動生成
+service('auth')->routes($routes);
